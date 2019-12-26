08d7fc3e63f33c55391d21aa6d8637abbd90d9fe
suryanshg <suryanshg47@gmail.com>
2019-12-26 20:21:06 +0530

    size() in LinkedList.py workinggit add .!

--- Python/LinkedList.py
+++ Python/LinkedList.py
@@ -1,15 +1,27 @@
+# Class to implement a node in a Linked List
 class Node:
     def __init__(self,data,next):
         self.data=data
         self.next=next
-    
+
+# Function to print a Linked List    
 def printLL(head):
     temp=head
     while(temp!=None):
         print(str(temp.data)+"->",end="")
         temp=temp.next
     print("NULL")
-    
+
+# Function that returns the size of a Linked List
+def size(head):
+    temp=head
+    count=0
+    while(temp!=None):
+        count+=1
+        temp=temp.next
+    return count
+
+# Driver Code
 if __name__=='__main__':
     third=Node(3,None)
     second=Node(2,third)
@@ -17,6 +29,7 @@ if __name__=='__main__':
 
     print("Original LinkedList is:")
     printLL(first)
+    print("Its size is:"+str(size(first)))
 
 
     
