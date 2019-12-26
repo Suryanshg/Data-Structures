99b0ae4695ce15c1fa0055a49ad35521588f8422
suryanshg <suryanshg47@gmail.com>
2019-12-26 20:14:40 +0530

    printLL in LinkedList.py working

--- Python/LinkedList.py
+++ Python/LinkedList.py
@@ -1,2 +1,22 @@
-print ("Hello World")
+class Node:
+    def __init__(self,data,next):
+        self.data=data
+        self.next=next
+    
+def printLL(head):
+    temp=head
+    while(temp!=None):
+        print(str(temp.data)+"->",end="")
+        temp=temp.next
+    print("NULL")
+    
+if __name__=='__main__':
+    third=Node(3,None)
+    second=Node(2,third)
+    first=Node(1,second)
+
+    print("Original LinkedList is:")
+    printLL(first)
+
+
     
