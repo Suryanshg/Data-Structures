5305d67536c769a26e9684c1eade72c134d758bb
suryanshg <suryanshg47@gmail.com>
2019-12-29 17:08:18 +0530

    printStack working

---
+++ Stack/Stack.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+class Stack{
+    public:
+    int* arr; // The array that holds the stack
+    int size; // The number of elements in the current stack
+    int top; // The top element of the array
+    int capacity; // Max Capacity of the stack
+
+    // Constructor
+    Stack(int input=10){
+        capacity=input;
+        size=0;
+        arr=new int[capacity];
+        top=-1;
+    } 
+
+    // Prints the stack
+    void printStack(){
+        int count=size-1;
+        while (count>=0){
+            cout<<arr[count]<<endl;
+            count--;
+        }
+    }
+
+    // Pushes an element at the top of the stack
+    void push(int val){
+        if(size>=capacity-1){
+            cout<<"Stack Overflow"<<endl;
+        }
+        else{
+            arr[size]=val;
+            top=val;
+            size++;
+        }
+    }
+
+
+
+
+};
+
+// Driver Code
+int main(){
+    Stack* S=new Stack(5);
+    S->push(1);
+    S->push(2);
+    S->push(3);
+    cout<<"The initial stack is:"<<endl;
+    S->printStack();
+}
