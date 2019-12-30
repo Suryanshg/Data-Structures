d80a08457cb50082a1bd901e83de9d9409bf0c8a
suryanshg <suryanshg47@gmail.com>
2019-12-30 20:29:22 +0530

    Added push() to Stack.py

---
+++ Stacks and Queues/Python/Stack.py
@@ -0,0 +1,37 @@
+class Stack:
+    # Constructor
+    def __init__(self,input):
+        self.capacity=input # Max Capacity of the stack
+        self.size=0 # The number of elements in the current stack
+        self.arr=[] # The array that holds the stack
+        self.top=-1 # The top element of the array
+    
+    #  Prints the stack
+    def printStack(self):
+        count=self.size-1
+        while (count>=0):
+            print("|"+str(self.arr[count])+"|")
+            count-=1
+        print("---")
+    
+    # Pushes an element at the top of the stack
+    def push(self,val):
+        if(self.size>self.capacity-1):
+            print("Stack Overflow")
+        else:
+            self.arr.append(val)
+            self.top=val
+            self.size+=1
+        
+    
+
+# Driver Code
+if __name__=='__main__':
+    S=Stack(5)
+    S.push(1)
+    S.push(2)
+    S.push(3)
+    S.push(4)
+    S.push(5)
+    print("The initial stack is:")
+    S.printStack()
