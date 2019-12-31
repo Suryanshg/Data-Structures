206d0ad3d703e84398936b425bb282b88a9eec13
suryanshg <suryanshg47@gmail.com>
2019-12-31 18:09:33 +0530

    Done with Queue.cpp

--- Stacks and Queues/CPP/Queue.cpp
+++ Stacks and Queues/CPP/Queue.cpp
@@ -3,39 +3,75 @@ using namespace std;
 
 class Queue{
     public:
-    int front,rear; // the front and rear elements in the queue respectively
+    int front,rear; // the front and rear element's index in the queue respectively
     int* arr; // the array that holds the elements of the queue
     int capacity; // the Max capacity of the queue  
-    int size; // the number of elements in the Queue
-
+    
     // Constructor
     Queue(int input=10){
         capacity=input;
         arr=new int[capacity];
-        size=0;
-        front=-1;
-        rear=-1;
+        front=0;
+        rear=0;
     }
 
     // Prints the Queue
     void printQ(){
-        for(int i=0;i<size;i++){
+        for(int i=0;i<rear;i++){
             cout<<"|"<<arr[i];
         }
-        cout<<"|"<<endl;
+
+        if (rear>0) cout<<"|";
+        cout<<endl;
     }
 
     // Adds the element at the back of the queue
     void enqueue(int val){
-        if(size>=capacity){
+        if(rear>=capacity){
             cout<<"Queue Overflow!"<<endl;
         }
         else{
-            arr[size]=val;
-            rear=val;
-            if(size==0){front=val;}
-            size++;
+            arr[rear]=val;
+            rear++;
         }
+    }
+
+    // Removes the element at the back of the queue
+    void dequeue(){
+        if(rear==0){
+            cout<<"Queue Underflow!"<<endl;
+        }
+        else{
+            for(int i=0; i<rear-1;i++){
+                arr[i]=arr[i+1];
+            }
+            rear--;
+        }
+    }
+
+    // Returns the element in front
+    int getFront(){
+        if(isEmpty()){
+            return -1;
+        }
+        else{
+            return arr[front];
+        }
+    }
+
+    // Returns the element in rear
+    int getRear(){
+        if(isEmpty()){
+            return -1;
+        }
+        else{
+            return arr[rear-1];
+        }
+    }
+
+    // Checks whether queue is empty or not
+    bool isEmpty(){
+        return rear==0;
     }
 
 };
@@ -48,7 +84,46 @@ int main(){
     Q->enqueue(3);
     Q->enqueue(4);
     Q->enqueue(5);
-    cout<<"Initial Queue is:"<<endl;
+    cout<<"Initial Queue of capacity 5 is:"<<endl;
     Q->printQ();
+    cout<<"Front: "<<Q->getFront()<<endl;
+    cout<<"Rear: "<<Q->getRear()<<endl;
 
+    cout<<"After adding 6 to the Queue: ";
+    Q->enqueue(6);
+
+    cout<<"After using dequeue:";
+    Q->dequeue();
+    Q->printQ();
+    cout<<"Front: "<<Q->getFront()<<endl;
+    cout<<"Rear: "<<Q->getRear()<<endl;
+
+    cout<<"After using dequeue:";
+    Q->dequeue();
+    Q->printQ();
+    cout<<"Front: "<<Q->getFront()<<endl;
+    cout<<"Rear: "<<Q->getRear()<<endl;
+
+    cout<<"After using dequeue:";
+    Q->dequeue();
+    Q->printQ();
+    cout<<"Front: "<<Q->getFront()<<endl;
+    cout<<"Rear: "<<Q->getRear()<<endl;
+
+    cout<<"After using dequeue:";
+    Q->dequeue();
+    Q->printQ();
+    cout<<"Front: "<<Q->getFront()<<endl;
+    cout<<"Rear: "<<Q->getRear()<<endl;
+
+    cout<<"After using dequeue:";
+    Q->dequeue();
+    Q->printQ();
+    cout<<"Front: "<<Q->getFront()<<endl;
+    cout<<"Rear: "<<Q->getRear()<<endl;
+
+    cout<<"After using dequeue:";
+    Q->dequeue();
+    cout<<"Front: "<<Q->getFront()<<endl;
+    cout<<"Rear: "<<Q->getRear()<<endl;
 }
